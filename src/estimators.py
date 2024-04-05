bd51aca763b1434dc6ae025aeb40f552fd6d504d
Gabriel Esteban Pinochet Soto <gpin2@pdx.edu>
2024-04-05 16:36:21 -0700

    Add DG setting

--- src/estimators.py
+++ src/estimators.py
@@ -2,9 +2,24 @@
 Error estimators implementation.
 """
 
-from ngsolve import GridFunction, Integrate, specialcf, InnerProduct, grad, x, y, z, dx
+from ngsolve import (
+    GridFunction,
+    Integrate,
+    specialcf,
+    InnerProduct,
+    grad,
+    x,
+    y,
+    z,
+    dx,
+    Mesh,
+)
 import numpy as np
 from numpy import sqrt
+
+from geo2d import make_unit_square
+from setup import setup_laplace
+from solver import solve_landscape
 
 
 def compute_avr_ngvecs(ngvecs, name="avg") -> GridFunction:
@@ -18,8 +33,8 @@ def compute_avr_ngvecs(ngvecs, name="avg") -> GridFunction:
     return gf
 
 
-def landscape_error_estimator(
-    ngvecs, order=None, potential=None, matrix_coeff=None, gamma=None
+def landscape_error_estimator_dg(
+    gf, order=None, potential=None, matrix_coeff=None, gamma=None
 ):
     """
     Compute the landscape error estimator.
@@ -29,16 +44,16 @@ def landscape_error_estimator(
     assert matrix_coeff is not None, "Matrix coefficient must be specified"
     assert gamma is not None, "Gamma must be specified"
 
-    gf = compute_avr_ngvecs(ngvecs)
+    # gf = compute_avr_ngvecs(ngvecs)
 
     h = specialcf.mesh_size
-    n = specialcf.normal(ngvecs.fes.mesh.dim)
+    n = specialcf.normal(gf.space.mesh.dim)
     p = order
-    xs = [x, y, z][: ngvecs.fes.mesh.dim]
+    xs = [x, y, z][: gf.space.mesh.dim]
 
     grad_gf = grad(gf)
     a_grad_gf = matrix_coeff * grad_gf
-    div_a_grad_gf = sum((a_grad_gf[i].Diff(xs[i]) for i in range(ngvecs.fes.mesh.dim)))
+    div_a_grad_gf = sum((a_grad_gf[i].Diff(xs[i]) for i in range(gf.space.mesh.dim)))
     v_gf = potential * gf
 
     # TODO: Up to a factor related to the matrix coefficient
@@ -50,39 +65,46 @@ def landscape_error_estimator(
     integrand_4 = (h / p + gamma**2 * p**3 / h) ** 0.5 * gf
 
     eta_1 = Integrate(
-        InnerProduct(integrand_1, integrand_1) * dx, ngvecs.fes.mesh, element_wise=True
+        InnerProduct(integrand_1, integrand_1) * dx, gf.space.mesh, element_wise=True
     )
     eta_2 = Integrate(
         InnerProduct(integrand_2, integrand_2) * dx(element_boundary=True),
-        ngvecs.fes.mesh,
+        gf.space.mesh,
         element_wise=True,
     )
     eta_3 = Integrate(
         InnerProduct(integrand_3, integrand_3) * dx(element_boundary=True),
-        ngvecs.fes.mesh,
+        gf.space.mesh,
         element_wise=True,
     )
     eta_4 = Integrate(
         InnerProduct(integrand_4, integrand_4) * dx(element_boundary=True),
-        ngvecs.fes.mesh,
+        gf.space.mesh,
         element_wise=True,
     )
 
-    eta = sqrt(
-        eta_1.real.NumPy()
-        + eta_2.real.NumPy()
-        + eta_3.real.NumPy()
-        + eta_4.real.NumPy()
-    )
+    eta = sqrt(eta_1.NumPy() + eta_2.NumPy() + eta_3.NumPy() + eta_4.NumPy())
     etas = {"eta_1": eta_1, "eta_2": eta_2, "eta_3": eta_3, "eta_4": eta_4}
     max_etas = {
-        "max_eta_1": np.max(eta_1.real.NumPy()),
-        "max_eta_2": np.max(eta_2.real.NumPy()),
-        "max_eta_3": np.max(eta_3.real.NumPy()),
-        "max_eta_4": np.max(eta_4.real.NumPy()),
+        "max_eta_1": np.max(eta_1.NumPy()),
+        "max_eta_2": np.max(eta_2.NumPy()),
+        "max_eta_3": np.max(eta_3.NumPy()),
+        "max_eta_4": np.max(eta_4.NumPy()),
     }
     return eta, etas, max_etas
 
 
 if __name__ == "__main__":
-    pass
+    print("Generating the mesh...\n")
+    ex_mesh = Mesh(make_unit_square().GenerateMesh(maxh=0.1))
+    print("Solving for the Laplace problem...\n")
+    matrix, mass, rhs, space = setup_laplace(ex_mesh, order=5)
+    print("Solving for the landscape function...\n")
+    landscape = solve_landscape(matrix, rhs, space)
+    print("Computing the landscape error estimator...\n")
+    # TODO: Do I need gamma here? Not using DG
+    eta, etas, max_etas = landscape_error_estimator_dg(
+        landscape, order=5, potential=0.0, matrix_coeff=1.0, gamma=1.0
+    )
+    print(f"Error estimator: {eta}")
+    print(f"Max error estimator components: {max_etas}")
--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@
 Set up different systems of PDEs
 """
 
-from ngsolve import H1, grad, dx, Mesh, Draw, BilinearForm, LinearForm
+from ngsolve import H1, grad, dx, Mesh, Draw, BilinearForm, LinearForm, L2, specialcf
 from geo2d import make_unit_square
 
 
@@ -44,6 +44,71 @@ def setup_helmholtz(mesh, coeff: float, order: int = 1, is_complex: bool = True)
     return a, m, f, fes
 
 
+def setup_adv_diff(
+    mesh, matrix_coeff=None, potential=None, order: int = 1, is_complex: bool = True
+):
+    """
+    Set up the advection-diffusion problem
+    """
+    assert matrix_coeff is not None, "Matrix coefficient must be provided"
+    assert potential is not None, "Potential must be provided"
+    # RHS
+    fes = H1(mesh, order=order, complex=is_complex, dirichlet="boundary")
+    u, v = fes.TnT()
+    a = BilinearForm(fes)
+    a += ((matrix_coeff * grad(u)) * grad(v) + potential * u * v) * dx
+    # LHS (for eigenvalue problem)
+    m = BilinearForm(fes)
+    m += u * v * dx
+    # Linear form (for the landscape function)
+    f = LinearForm(fes)
+    f += v * dx
+    assemble(a, m, f)
+    return a, m, f, fes
+
+
+def setup_adv_diff_dg(
+    mesh,
+    matrix_coeff=None,
+    potential=None,
+    gamma=None,
+    order: int = 1,
+    is_complex: bool = True,
+):
+    """
+    Set up the advection-diffusion problem with discontinuous Galerkin
+    """
+    assert matrix_coeff is not None, "Matrix coefficient must be provided"
+    assert potential is not None, "Potential must be provided"
+    assert gamma is not None, "Jump penalty must be provided"
+
+    fes = L2(mesh, order=order, complex=is_complex, dgjumps=True)
+    u, v = fes.TnT()
+    n = specialcf.normal(mesh.dim)
+    h = specialcf.mesh_size
+
+    jump_u = u - u.Other()
+    jump_v = v - v.Other()
+    mean_grad_u = 0.5 * n * (matrix_coeff * grad(u) + matrix_coeff * grad(u.Other()))
+    mean_grad_v = 0.5 * n * (matrix_coeff * grad(v) + matrix_coeff * grad(v.Other()))
+
+    a = BilinearForm(fes)
+    a += matrix_coeff * grad(u) * grad(v) * dx
+    a += potential * u * v * dx
+    a += -mean_grad_u * jump_v * dx(skeleton=True)
+    a += -mean_grad_v * jump_u * dx(skeleton=True)
+    # TODO: Define c
+    a += order**2 * gamma / h * jump_u * jump_v * dx(skeleton=True)
+
+    m = BilinearForm(fes)
+    m += u * v * dx
+
+    f = LinearForm(fes)
+    f += v * dx
+    assemble(a, m, f)
+    return a, m, f, fes
+
+
 def assemble(*args) -> None:
     """
     Assemble the forms
@@ -71,6 +136,24 @@ if __name__ == "__main__":
     input("Press Enter to continue...")
 
     matrix, mass, rhs, space = setup_helmholtz(ex_mesh, 1.0)
+    print(
+        f"Number of DOFs: {space.ndof}"
+        f"Bilinear form a: {matrix.mat}"
+        f"Linear form f: {rhs.vec}"
+        f"Mass form m: {mass.mat}"
+    )
+    input("Press Enter to continue...")
+
+    matrix, mass, rhs, space = setup_adv_diff(ex_mesh, 1.0, 1.0)
+    print(
+        f"Number of DOFs: {space.ndof}"
+        f"Bilinear form a: {matrix.mat}"
+        f"Linear form f: {rhs.vec}"
+        f"Mass form m: {mass.mat}"
+    )
+    input("Press Enter to continue...")
+
+    matrix, mass, rhs, space = setup_adv_diff_dg(ex_mesh, 1.0, 1.0, 1.0)
     print(
         f"Number of DOFs: {space.ndof}"
         f"Bilinear form a: {matrix.mat}"
