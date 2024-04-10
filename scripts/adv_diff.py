bd985f0ff538f5826cf055bf67786500143f4bb6
Gabriel Esteban Pinochet Soto <gpin2@pdx.edu>
2024-04-10 15:20:47 -0700

    Add working adaptivity

--- scripts/adv_diff.py
+++ scripts/adv_diff.py
@@ -1,7 +1,9 @@
 """
-ARD equation in 2D on a unit square domain with Dirichlet boundary conditions. """
+ARD equation in 2D on a unit square domain with Dirichlet boundary conditions.
+"""
 
 import numpy as np
+import pandas as pd
 from ngsolve import (
     Draw,
     H1,
@@ -160,15 +162,15 @@ def get_default_values_feast(**kwargs):
     """
     defaults = {
         "center": 1.0,
-        "radius": 1.0,
-        "nspan": 4,
+        "radius": 0.1,
+        "nspan": 2,
         "npts": 4,
         "checks": False,
         "seed": 1,
         "hermitian": False,
-        "stop_tol": 1e-12,
-        "cut_tol": 1e-14,
-        "nrestarts": 10,
+        "stop_tol": 1e-11,
+        "cut_tol": 1e-11,
+        "nrestarts": 5,
         "niterations": 100,
         "verbose": True,
     }
@@ -179,7 +181,9 @@ def get_default_values_feast(**kwargs):
 
 
 # Error estimator
-def error_estimator(gf, matrix_coeff=None, vector_coeff=None, scalar_coeff=None):
+def error_estimator_landscape(
+    gf, matrix_coeff=None, vector_coeff=None, scalar_coeff=None
+):
     """
     Compute the landscape error estimator.
     """
@@ -216,6 +220,45 @@ def error_estimator(gf, matrix_coeff=None, vector_coeff=None, scalar_coeff=None)
     return eta, etas, max_etas
 
 
+def error_estimator_eigenfunction(
+    gf, ev, matrix_coeff=None, vector_coeff=None, scalar_coeff=None
+):
+    """
+    Compute the eigenfunction error estimator.
+    """
+    assert matrix_coeff is not None, "Matrix coefficient must be provided"
+    assert vector_coeff is not None, "Vector coefficient must be provided"
+    assert scalar_coeff is not None, "Scalar coefficient must be provided"
+    h = specialcf.mesh_size
+    n = specialcf.normal(gf.space.mesh.dim)
+    xs = [x, y, z][: gf.space.mesh.dim]
+
+    grad_gf = grad(gf)
+    mat_grad_gf = matrix_coeff * grad_gf
+    vec_grad_gf = vector_coeff * grad_gf
+    div_grad_gf = sum((mat_grad_gf[i].Diff(xs[i]) for i in range(gf.space.mesh.dim)))
+
+    integrand_1 = -div_grad_gf + vec_grad_gf + scalar_coeff * gf - ev * gf
+    integrand_2 = 0.5**0.5 * h**0.5 * (mat_grad_gf - mat_grad_gf.Other()) * n
+
+    eta_1 = Integrate(
+        InnerProduct(integrand_1, integrand_1) * dx, gf.space.mesh, element_wise=True
+    )
+    eta_2 = Integrate(
+        InnerProduct(integrand_2, integrand_2) * dx(element_boundary=True),
+        gf.space.mesh,
+        element_wise=True,
+    )
+
+    eta = np.sqrt(eta_1.NumPy().real + eta_2.NumPy().real)
+    etas = {"eta_1": eta_1, "eta_2": eta_2}
+    max_etas = {
+        "max_eta_1": np.max(eta_1.NumPy()),
+        "max_eta_2": np.max(eta_2.NumPy()),
+    }
+    return eta, etas, max_etas
+
+
 # Marking
 def mark(mesh, eta, theta=0.5):
     """
@@ -226,6 +269,7 @@ def mark(mesh, eta, theta=0.5):
 
 
 # Full adaptivity routine
+# TODO: Add error decresing
 def adaptivity(order: int = 1, maxiter: int = 5, theta: float = 0.9):
     """
     Adaptivity routine for a given geometry, setup, estimator and parameters.
@@ -246,6 +290,8 @@ def adaptivity(order: int = 1, maxiter: int = 5, theta: float = 0.9):
         is_complex=True,
     )
     iteration = 0
+    etas = []
+    ndofs = []
     while iteration < maxiter:
         iteration += 1
         Draw(mesh)
@@ -253,9 +299,12 @@ def adaptivity(order: int = 1, maxiter: int = 5, theta: float = 0.9):
         # Solve
         u = solve(a, f, fes)
         # Estimate
-        eta, _, _ = error_estimator(
+        eta, _, _ = error_estimator_landscape(
             u, matrix_coeff=matrix, vector_coeff=vector, scalar_coeff=scalar
         )
+        # Save info
+        etas.append(eta)
+        ndofs.append(fes.ndof)
         # Mark
         mark(mesh, eta, theta=theta)
         # Refine
@@ -271,20 +320,104 @@ def adaptivity(order: int = 1, maxiter: int = 5, theta: float = 0.9):
         )
     Draw(u, mesh, name="Solution")
     input("Press any key to continue...")
-    return u, mesh
+    info = {
+        "etas": etas,
+        "ndofs": ndofs,
+    }
+    return u, mesh, info
 
 
 def adaptivity_eigenvalue(
     order: int = 1, maxiter: int = 5, theta: float = 0.9, **kwargs
 ):
     """
-    TBA
+    Adaptivity routine for a given geometry, setup, estimator and parameters.
+    It refines w.r.t. the error estimator for the eigenvalue problem.
     """
+    assert 0 < theta < 1, "Theta must be in (0, 1)"
+    assert maxiter > 0, "Maxiter must be positive"
+    matrix = CoefficientFunction((1, 0, 0, 1), dims=(2, 2))
+    vector = CoefficientFunction((20, 30))
+    scalar = CoefficientFunction(0.0)
+    mesh = Mesh(make_unit_square().GenerateMesh(maxh=0.3))
+    iteration = 0
+    etas = []
+    error = []
+    ndofs = []
+    lambdas = [0.0]
+    center = kwargs.get("center")
+    kwargs.pop("center")
+    while iteration < maxiter:
+        iteration += 1
+        Draw(mesh)
+        # Update the forms
+        a, m, _, fes = get_forms(
+            mesh,
+            matrix_coeff=matrix,
+            vector_coeff=vector,
+            scalar_coeff=scalar,
+            order=order,
+            is_complex=True,
+        )
+        # input("Press any key...")
+        # Solve
+        right, left, evals = solve_eigenvalue(a, m, fes, center=center, **kwargs)
+        # Estimate
+        estimators = []
+        for i in range(right.m):
+            u = right[i]
+            v = left[i]
+            eta_r, _, _ = error_estimator_eigenfunction(
+                u,
+                evals[i],
+                matrix_coeff=matrix,
+                vector_coeff=vector,
+                scalar_coeff=scalar,
+            )
+            eta_l, _, _ = error_estimator_eigenfunction(
+                v,
+                evals[i],
+                matrix_coeff=matrix,
+                vector_coeff=vector,
+                scalar_coeff=scalar,
+            )
+            estimators.append(eta_r)
+            estimators.append(eta_l)
+        # Take the maximum error estimator
+        eta = np.max(estimators, axis=0)
+        # Save info
+        etas.append(eta)
+        lambdas.append(np.mean(evals))
+        error.append(np.absolute(lambdas[-1] - lambdas[-2]))
+        ndofs.append(fes.ndof)
+        # Update the center
+        center = lambdas[-1]
+        # Mark
+        mark(mesh, eta, theta=theta)
+        # Refine
+        if iteration < maxiter:
+            mesh.Refine()
+    print(f"Eigenvalues: {evals}")
+    info = {
+        "etas": etas,
+        "ndofs": ndofs,
+        "lambdas": lambdas[1:],
+        "error": error,
+    }
+    return right, left, evals, mesh, info
 
 
-####################################################################################################
-# Tests ############################################################################################
-####################################################################################################
+def to_file(info, filename):
+    """
+    Use pandas to save the info to a file.
+    """
+    df = pd.DataFrame(info)
+    df.to_csv(filename)
+
+
+#########################################################################################
+# Tests #################################################################################
+#########################################################################################
 
 ORDER = 3
 MAXITER = 30
@@ -292,18 +425,19 @@ THETA = 0.9
 
 CENTER = 5 * np.pi**2 + 325
 RADIUS = 1.0
-NPTS = 4
-NSPAN = 4
+NPTS = 6
+NSPAN = 2
 
 
-def test_adaptivity():
+def test_adaptivity_landscape():
     """
     Test the adaptivity routine for the landscape problem, with the error estimator.
     """
     matrix = CoefficientFunction((1, 0, 0, 1), dims=(2, 2))
     vector = CoefficientFunction((20, 30))
     scalar = CoefficientFunction(0.0)
-    _, mesh = adaptivity(order=ORDER, maxiter=MAXITER, theta=THETA)
+    _, mesh, info = adaptivity(order=ORDER, maxiter=MAXITER, theta=THETA)
+    to_file(info, "landscape_info.csv")
     a, m, _, fes = get_forms(
         mesh, order=ORDER, matrix_coeff=matrix, vector_coeff=vector, scalar_coeff=scalar
     )
@@ -319,5 +453,116 @@ def test_adaptivity():
     print("Done!")
 
 
+def test_adaptivity_eigenvalue():
+    """
+    Test the adaptivity routine for the eigenvalue problem, with the error estimator.
+    """
+    matrix = CoefficientFunction((1, 0, 0, 1), dims=(2, 2))
+    vector = CoefficientFunction((20, 30))
+    scalar = CoefficientFunction(0.0)
+    right, left, evals, mesh, info = adaptivity_eigenvalue(
+        order=ORDER,
+        maxiter=MAXITER,
+        theta=THETA,
+        center=CENTER,
+        radius=RADIUS,
+        npts=NPTS,
+        nspan=NSPAN,
+    )
+    to_file(info, "eigenvalue_info.csv")
+    for k in range(right.m):
+        Draw(right[k], mesh, name=f"Eigenfunction {k}")
+        Draw(left[k], mesh, name=f"Left Eigenfunction {k}")
+        input(f"\tPress any key to continue... Eigenvalue {k}: {evals[k]}")
+    a, _, f, fes = get_forms(
+        mesh, order=ORDER, matrix_coeff=matrix, vector_coeff=vector, scalar_coeff=scalar
+    )
+    u = solve(a, f, fes)
+    Draw(u, mesh, name="Landscape solution")
+    print(f"Eigenvalues: {evals}")
+    print(f"Number of eigenvalues: {len(evals)}")
+    print("Done!")
+
+
+def test_adaptivity_landscape_ev():
+    """
+    Test the adaptivity routine for the landscape problem, with the error estimator.
+    We compute the eigenvalues and eigenfunctions of the landscape problem, per each iteration.
+    """
+    matrix = CoefficientFunction((1, 0, 0, 1), dims=(2, 2))
+    vector = CoefficientFunction((20, 30))
+    scalar = CoefficientFunction(0.0)
+
+    mesh = Mesh(make_unit_square().GenerateMesh(maxh=0.2))
+
+    iteration = 0
+    etas_max = []
+    etas_l2 = []
+    etas = []
+    ndofs = []
+    avg_evals = []
+    avg_evals_real = []
+    avg_evals_imag = []
+    error_avg = []
+    error = []
+    center = CENTER
+    while iteration < MAXITER:
+        iteration += 1
+        # Update the forms
+        a, m, f, fes = get_forms(
+            mesh,
+            matrix_coeff=matrix,
+            vector_coeff=vector,
+            scalar_coeff=scalar,
+            order=ORDER,
+            is_complex=True,
+        )
+        Draw(mesh)
+        # Solve
+        u = solve(a, f, fes)
+        # Solve eigenvalue problem
+        _, _, evals = solve_eigenvalue(a, m, fes, center=center)
+        # Estimate
+        eta, _, _ = error_estimator_landscape(
+            u, matrix_coeff=matrix, vector_coeff=vector, scalar_coeff=scalar
+        )
+        # Save info
+        etas.append(eta)
+        etas_max.append(np.max(eta))
+        etas_l2.append(np.linalg.norm(eta))
+        ndofs.append(fes.ndof)
+        avg_evals.append(np.mean(evals))
+        avg_evals_real.append(np.real(np.mean(evals)))
+        avg_evals_imag.append(np.imag(np.mean(evals)))
+        error_avg.append(np.absolute(avg_evals[-1] - CENTER))
+        error.append(np.absolute(np.array(evals) - CENTER))
+        # Update the center
+        center = avg_evals[-1]
+        # Mark
+        mark(mesh, eta, theta=THETA)
+        # Refine
+        mesh.Refine()
+    Draw(u, mesh, name="Solution")
+    input("Press any key to continue...")
+    info = {
+        # "etas": etas,
+        "etas_max": etas_max,
+        "etas_l2": etas_l2,
+        "ndofs": ndofs,
+        "avg_evals": avg_evals,
+        "avg_evals_real": avg_evals_real,
+        "avg_evals_imag": avg_evals_imag,
+        "error_avg": error_avg,
+        # "error": error,
+    }
+    to_file(info, "lands_ev_info.csv")
+
+
 if __name__ == "__main__":
-    test_adaptivity()
+    # test_adaptivity_landscape()
+    # print("\n\n\nLandscape done!\n\n\n")
+    # test_adaptivity_eigenvalue()
+    # print("\n\n\nEigenvalue done!\n\n\n")
+    test_adaptivity_landscape_ev()
+    print("\n\n\nLandscape and eigenvalues done!\n\n\n")
+    print("All tests done!")
